478087ada320bcceb17fac2e8ede8da276bc9d6d
James Page <james.page@canonical.com>
2013-10-14 17:10:30 +0100

    Rebase on charmhelpers for havana

--- hooks/ceilometer_utils.py
+++ hooks/ceilometer_utils.py
@@ -1,28 +1,36 @@
 import os
 import uuid
-import ConfigParser
+from charmhelpers.fetch import apt_install as install
 
 RABBIT_USER = "ceilometer"
 RABBIT_VHOST = "ceilometer"
 CEILOMETER_CONF = "/etc/ceilometer/ceilometer.conf"
 
 SHARED_SECRET = "/etc/ceilometer/secret.txt"
-CEILOMETER_SERVICES = ['ceilometer-agent-central', 'ceilometer-collector',
-    'ceilometer-api']
+CEILOMETER_SERVICES = [
+    'ceilometer-agent-central', 'ceilometer-collector',
+    'ceilometer-api'
+]
 CEILOMETER_DB = "ceilometer"
 CEILOMETER_SERVICE = "ceilometer"
 CEILOMETER_COMPUTE_SERVICES = ['ceilometer-agent-compute']
-CEILOMETER_PACKAGES = ['python-ceilometer', 'ceilometer-common',
-    'ceilometer-agent-central', 'ceilometer-collector', 'ceilometer-api']
-CEILOMETER_AGENT_PACKAGES = ['python-ceilometer', 'ceilometer-common',
-    'ceilometer-agent-compute']
+CEILOMETER_PACKAGES = [
+    'python-ceilometer', 'ceilometer-common',
+    'ceilometer-agent-central', 'ceilometer-collector', 'ceilometer-api'
+]
+CEILOMETER_AGENT_PACKAGES = [
+    'python-ceilometer', 'ceilometer-common',
+    'ceilometer-agent-compute'
+]
 CEILOMETER_PORT = 8777
+CEILOMETER_ROLE = "ResellerAdmin"
 
 NOVA_CONF = "/etc/nova/nova.conf"
 NOVA_SETTINGS = [
     ('DEFAULT', 'instance_usage_audit', 'True'),
     ('DEFAULT', 'instance_usage_audit_period', 'hour'),
-    ('DEFAULT', 'notification_driver', 'ceilometer.compute.nova_notifier')]
+    ('DEFAULT', 'notification_driver', 'ceilometer.compute.nova_notifier')
+]
 
 
 def get_shared_secret():
@@ -37,28 +45,17 @@ def get_shared_secret():
     return secret
 
 
-def modify_config_file(nova_conf, values):
-    # check if config file exists
-    if not os.path.exists(nova_conf):
-        juju_log('ERROR', 'nova config file must exist at this point')
-        sys.exit(1)
+TEMPLATES_DIR = 'templates'
 
-    try:
-        config = ConfigParser.ConfigParser()
-        with open(nova_conf, "r") as f:
-            config.readfp(f)
+try:
+    import jinja2
+except ImportError:
+    install(['python-jinja2'])
+    import jinja2
 
-        # add needed config lines - tuple with section,key,value
-        for value in values:
-            config.set(value[0], value[1], value[2])
 
-        with open(nova_conf, "w") as f:
-            config.write(f)
-
-        # add line at the end, it's a dupe and configparser doesn't handle it
-        with open(nova_conf, "a") as f:
-            f.write("notification_driver=nova.openstack.common.notifier.rabbit_notifier\n")
-
-    except IOError as e:
-        juju_log('ERROR', 'Error updating nova config file')
-        sys.exit(1)
+def render_template(template_name, context, template_dir=TEMPLATES_DIR):
+    templates = \
+        jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
+    template = templates.get_template(template_name)
+    return template.render(context)
