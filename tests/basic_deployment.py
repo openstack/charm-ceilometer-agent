861de659573129a8456955000b4bbb04672fcb3e
James Page <james.page@ubuntu.com>
2015-11-02 09:52:06 +0000

    Add tox support

--- tests/basic_deployment.py
+++ tests/basic_deployment.py
@@ -468,7 +468,7 @@ class CeiloAgentBasicDeployment(OpenStackAmuletDeployment):
                 'auth_uri': auth_uri,
                 'auth_host': ks_rel['auth_host'],
                 'auth_port': ks_rel['auth_port'],
-                'auth_protocol':  ks_rel['auth_protocol'],
+                'auth_protocol': ks_rel['auth_protocol'],
                 'admin_tenant_name': 'services',
                 'admin_user': 'ceilometer',
                 'admin_password': ks_rel['service_password'],
