9027a7b8941f75f2924869de2f821ee7283ccce9
Camille Rodriguez <camille.rodriguez@canonical.com>
2019-08-26 15:33:52 -0400

    Add disco-stein bundle to gate tests
    The bundle disco-stein was in the dev bundles because
    a bug with the Python 3.7 syntax prevented the success of
    the deployment of HA clusters. This bug is fixed, we can
    reintegrate disco-stein in the gate bundles.
    
    Other small additions:
    - removing sitepackages in tox.ini to avoid test env pollution
    - skip_missing_interpreters in tox.ini set to False to avoid false
    positives by skipping missing interpreters.
    
    LP Related-Bug: #1823718
    
    Change-Id: Ica4bead71d054ae5912e7e66ff9a0666e131637a

--- tests/basic_deployment.py
+++ tests/basic_deployment.py
@@ -76,7 +76,8 @@ class CeiloAgentBasicDeployment(OpenStackAmuletDeployment):
         if self._get_openstack_release() >= self.xenial_pike:
             other_services.extend([
                 {'name': 'gnocchi'},
-                {'name': 'memcached', 'location': 'cs:memcached'},
+                {'name': 'memcached',
+                 'location': 'cs:~memcached-team/memcached'},
                 {'name': 'ceph-mon', 'units': 3},
                 {'name': 'ceph-osd', 'units': 3,
                  'storage': {'osd-devices': 'cinder,10G'}}])
